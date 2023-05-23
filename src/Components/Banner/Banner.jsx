3067a2ce5c35a0a7ccfe6f1ace1e414c296a2dde
AeRyZe <laurent.charleval@free.fr>
2023-05-23 14:03:02 +0200

    feat(banner): add banner comp

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import './Banner.css'
+
+function Banner({ title, banner }) {
+    return (
+        <div className='banner-container'>
+            <img src={banner} className='banner-img' alt='Bannière à propos Kasa'></img>
+            <div className='banner-overlay'>
+                <h1 className='banner-title'>{title}</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,11 @@
+import Banner from "../../Components/Banner/Banner.jsx"
+import banner from '../../Assets/banner1.png'
+
 function Home() {
     return (
-        <h1>Page d'accueil</h1>
+        <main>
+            <Banner title='Chez vous, partout et ailleurs' banner={banner} />
+        </main>
     )
 }
 
