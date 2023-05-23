1e3e611f43a609a5eb4167ef406297e212ebc479
AeRyZe <laurent.charleval@free.fr>
2023-05-23 20:02:09 +0200

    feat(404): add 404 error page

--- src/Pages/Error404/Error404.jsx
+++ src/Pages/Error404/Error404.jsx
@@ -1,6 +1,17 @@
+import './Error404.css'
+import { Link } from 'react-router-dom'
+
 function Error404() {
     return (
-        <h1>Error404</h1>
+        <main>
+            <div className='error-container'>
+                <div className='error-message'>
+                    <h1 className='error-404'>404</h1>
+                    <h2 className='error-desc'>Oups! La page que vous demandez n'existe pas.</h2>
+                </div>
+                <Link to="/">Retour à la page d'accueil</Link>
+            </div>
+        </main>
     )
 }
 
