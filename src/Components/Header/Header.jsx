147e8560f5c95f337c64458b646ed358b3d2331b
AeRyZe <laurent.charleval@free.fr>
2023-05-23 14:02:21 +0200

    fix(header): header fix

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom"
-import Logo from '../../Assets/logo.png'
+import logo from '../../Assets/logo.png'
 import './Header.css'
 
 function Header() {
     return (
         <header>
-            <img src={Logo} alt="Logo de Kasa" />
+            <img src={logo} alt="Logo de Kasa" />
             <nav>
                 <Link to="/">Accueil</Link>
                 <Link to="/a-propos">À propos</Link>
