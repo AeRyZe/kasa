94fcbbf5563edad59f7012e013676f646241fcbd
AeRyZe <laurent.charleval@free.fr>
2023-05-23 11:27:01 +0200

    feat(app): add header with style

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,16 @@
+import { Link } from "react-router-dom"
+import Logo from '../../Assets/logo.png'
+import './Header.css'
+
 function Header() {
     return (
-        <h1>Header</h1>
+        <header>
+            <img src={Logo} alt="Logo de Kasa" />
+            <nav>
+                <Link to="/">Accueil</Link>
+                <Link to="/a-propos">À propos</Link>
+            </nav>
+        </header>
     )
 }
 
