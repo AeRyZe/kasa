75fa19c71d723df1b2a4d3c945fc7a47aa69e75b
AeRyZe <laurent.charleval@free.fr>
2023-05-24 04:42:46 +0200

    fix(thumb): thumb comp usable with props

--- src/Components/Thumb/Thumb.jsx
+++ src/Components/Thumb/Thumb.jsx
@@ -1,21 +1,16 @@
-import data from '../../logements.json'
 import './Thumb.css'
 import { Link } from 'react-router-dom'
 
-function Thumb() {
+function Thumb({ id, title, cover }) {
     return (
-        <div className='thumbs-container'>
-            {data.map((item) => (
-                <Link key={`link-${item.id}`} to={`/logement/${item.id}`}>
-                    <div key={`thumb-${item.id}`} className='thumb'>
-                        <img key={`img-${item.id}`} src={item.cover} alt="Couverture de logement" />
-                        <div key={`gradient-${item.id}`} className='gradient'>
-                            <p key={`title-${item.id}`}>{item.title}</p>
-                        </div>
-                    </div>
-                </Link>
-            ))}
-        </div>
+        <Link key={`link-${id}`} to={`/logement/${id}`} className='thumb-link'>
+            <div key={`thumb-${id}`} className='thumb'>
+                <img key={`img-${id}`} src={cover} alt="Couverture de logement" />
+                <div key={`gradient-${id}`} className='gradient'>
+                    <p key={`title-${id}`}>{title}</p>
+                </div>
+            </div>
+        </Link>
     )
 }
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,18 @@
 import Banner from "../../Components/Banner/Banner.jsx"
 import banner from '../../Assets/banner1.png'
 import Thumb from "../../Components/Thumb/Thumb.jsx"
+import './Home.css'
+import data from '../../logements.json'
 
 function Home() {
     return (
         <main>
             <Banner title='Chez vous, partout et ailleurs' banner={banner} />
-            <Thumb />
+            <div className="thumbs-container">
+                {data.map((item) => (
+                    <Thumb key={`thumbcomp-${item.id}`} id={item.id} title={item.title} cover={item.cover} />
+                ))}
+            </div>
         </main>
     )
 }
