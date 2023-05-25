e5e709fa8c5a6ce63d527013fbd23f37b5826535
AeRyZe <laurent.charleval@free.fr>
2023-05-25 15:47:22 +0200

    feat(housing): add housing page & connected to app

--- src/Pages/Housing/Housing.jsx
+++ src/Pages/Housing/Housing.jsx
@@ -1,6 +1,49 @@
+import logements from '../../logements.json'
+import './Housing.css'
+import { Navigate, useParams } from 'react-router-dom'
+import Carousel from '../../Components/Carousel/Carousel'
+import Host from '../../Components/Host/Host'
+import Tag from '../../Components/Tag/Tag'
+import Rating from '../../Components/Rating/Rating'
+import Collapse from '../../Components/Collapse/Collapse'
+
 function Housing() {
+    const { housingId } = useParams();
+    const obj = logements.find(object => object.id === housingId);
+
     return (
-        <h1>Logement</h1>
+        <main>
+            {obj !== undefined ? (
+                <div className='housing-container'>
+                    <Carousel Array={obj.pictures} />
+                    <div className='housing-first-layer'>
+                        <h1 className='housing-title'>{obj.title}</h1>
+                        <Host name={obj.host.name} pic={obj.host.picture} />
+                    </div>
+                    <p className='housing-location'>{obj.location}</p>
+                    <div className='housing-second-layer'>
+                        <div className='housing-tags'>
+                            {obj.tags.map((item, index) => (
+                                <Tag key={`housetag-${obj.id}-${index}`} value={item} />
+                            ))}
+                        </div>
+                        <div className='housing-rating'>
+                            <Rating averageRating={parseInt(obj.rating)} />
+                        </div>
+                    </div>
+                    <div className='housing-third-layer'>
+                        <div className='housing-collapse'>
+                            <Collapse type='string' title='Description' value={obj.description} />
+                        </div>
+                        <div className='housing-collapse'>
+                            <Collapse type='array' title='Équipements' value={obj.equipments} />
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <Navigate to='/not-found' />
+            )}
+        </main>
     )
 }
 
