4a7c8c1b0d347c22dba17c87bf51e48b2d804107
AeRyZe <laurent.charleval@free.fr>
2023-05-29 14:54:50 +0200

    ref(carousel): next&prev img code more accurate

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -5,22 +5,22 @@ import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 
 function Carousel({ Array }) {
-    const [index, setIndex] = useState(1)
+    const [index, setIndex] = useState(0)
 
     const nextImg = () => {
-        index >= Array.length ? setIndex(1) : setIndex(index + 1)
+        index === Array.length - 1 ? setIndex(0) : setIndex(index + 1)
     };
     
     const prevImg = () => {
-        index <= 1 ? setIndex(Array.length) : setIndex(index - 1)
+        index === 0 ? setIndex(Array.length - 1) : setIndex(index - 1)
     };
 
     return (
         <div className='carousel-container'>
-            <img src={Array[index - 1]} alt="Un logement proposé sur Kasa" className='carousel-currentimg' />
+            <img src={Array[index]} alt="Un logement proposé sur Kasa" className='carousel-currentimg' />
             <div className='carousel-nav'>
                 <FontAwesomeIcon icon={faChevronLeft} size='2xl' className='carousel-btn' onClick={prevImg} />
-                <p className='carousel-index'>{`${index}/${Array.length}`}</p>
+                <p className='carousel-index'>{`${index + 1}/${Array.length}`}</p>
                 <FontAwesomeIcon icon={faChevronRight} size='2xl' className='carousel-btn' onClick={nextImg} />
             </div>
         </div>
