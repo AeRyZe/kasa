8f523dfdae3691f0c687ef44f7d4ac5f13329552
AeRyZe <laurent.charleval@free.fr>
2023-05-29 14:56:19 +0200

    ref(rating): updated with max rating prop & map

--- src/Components/Rating/Rating.jsx
+++ src/Components/Rating/Rating.jsx
@@ -2,14 +2,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faStar } from "@fortawesome/free-solid-svg-icons"
 import './Rating.css'
 
-function Rating({ averageRating }) {
+function Rating({ averageRating, max }) {
+    const ratingArray = Array(max).fill('');
+
     return (
         <div className="rating-container">
-            <FontAwesomeIcon icon={faStar} size="lg" className={averageRating >= 1 ? 'rating-star-full' : 'rating-star-empty'} />
-            <FontAwesomeIcon icon={faStar} size="lg" className={averageRating >= 2 ? 'rating-star-full' : 'rating-star-empty'} />
-            <FontAwesomeIcon icon={faStar} size="lg" className={averageRating >= 3 ? 'rating-star-full' : 'rating-star-empty'} />
-            <FontAwesomeIcon icon={faStar} size="lg" className={averageRating >= 4 ? 'rating-star-full' : 'rating-star-empty'} />
-            <FontAwesomeIcon icon={faStar} size="lg" className={averageRating >= 5 ? 'rating-star-full' : 'rating-star-empty'} />
+            {ratingArray.map((val, i) => (
+                    <FontAwesomeIcon key={`ratingcomp-${i}`} icon={faStar} size="lg" className={i + 1 <= averageRating ? 'rating-star-full' : 'rating-star-empty'} />
+                ))}
         </div>
     )
 }
