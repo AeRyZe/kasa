f4f7ebea55f8eb7073b7d3712f519fdbd0038a10
AeRyZe <laurent.charleval@free.fr>
2023-05-29 15:00:58 +0200

    ref(home): updated data import with async await

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,14 +2,25 @@ import Banner from "../../Components/Banner/Banner.jsx"
 import banner from '../../Assets/banner1.png'
 import Thumb from "../../Components/Thumb/Thumb.jsx"
 import './Home.css'
-import data from '../../logements.json'
+import { useEffect, useState } from "react"
 
 function Home() {
+    const [housings, setHousings] = useState([])
+
+    useEffect(() => {
+        async function getHousings() {
+            const results = await fetch('http://localhost:3000/logements.json')
+            const data = await results.json()
+            setHousings(data)
+        }
+        getHousings()
+    }, [])
+
     return (
         <main>
             <Banner title='Chez vous, partout et ailleurs' banner={banner} clazzName={'home-banner-container'} />
             <div className="thumbs-container">
-                {data.map((item) => (
+                {housings.map((item) => (
                     <Thumb key={`thumbcomp-${item.id}`} id={item.id} title={item.title} cover={item.cover} />
                 ))}
             </div>
